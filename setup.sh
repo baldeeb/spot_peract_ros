2c4d84079f337cb1e73eaf7e779a2a01aca3a577
Bahaa Aldeeb <aldeeb.bahaa@gmail.com>
2023-07-26 14:00:55 -0500

    Minor tweaks.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ENV_NAME=spot-ros-peract
+ENV_NAME=spot-peract-ros
 
 # Pull spot_peract into src folder
 git submodule update --init --recursive
 
+
 # Create conda env called ros_peract
-conda create --prefix ${SCRIPT_DIR}/env -n ${ENV_NAME} python=3.8
-conda activate ${ENV_NAME}
-pip install -r requirements.txt
+if conda info --envs | grep ${SCRIPT_DIR}/env/${ENV_NAME}; 
+then echo "env already exists"; 
+else conda create --prefix ${SCRIPT_DIR}/env/${ENV_NAME} python=3.8 -y;
+fi
+
+# conda create --prefix ${SCRIPT_DIR}/env/${ENV_NAME} python=3.8 -y
+eval "$(conda shell.bash hook)"
+conda activate ${SCRIPT_DIR}/env/${ENV_NAME}
+# pip install -r requirements.txt
 
 # Setup PerAct
-cd ${SCRIPT_DIR}/peract_ros_wrapper/src/spot_peract
-./setup.sh
+source ${SCRIPT_DIR}/peract_ros_wrapper/src/spot_peract/setup.sh
